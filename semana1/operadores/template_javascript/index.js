ea7c27344aab54c3b4e40cc37bba7021a922179b
cibelefran <fran.cffs@gmail.com>
2021-06-15 16:50:11 -0300

    vs code

---
+++ semana1/operadores/template_javascript/index.js
@@ -0,0 +1,7 @@
+//Exercícios de interpretação de codigo
+//a. false b.false c.
+//Exercícios de escrita de código
+//1.
+const idadeDoUsuario = prompt(Number('Qual a sua idade?'))
+const idadeDaMelhorAmiga = prompt(Number('Qual a idade da sua melhor amiga?'))
+
