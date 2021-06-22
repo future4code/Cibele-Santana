914d4e921684449d97cecd8f4511d2c88bdd15d8
cibelefran <fran.cffs@gmail.com>
2021-06-22 14:48:54 -0300

    atualização

---
+++ semana2/objetos/index.js
@@ -0,0 +1,17 @@
+//Exercícios de interpretação:
+// 1. A) Matheus Nachtergaele, 
+//      3 ou Denise Fraga 
+//      canal: "Canal Brasil", horario: "19h"
+
+// 2. nome: "Juca", 
+//	idade: 3, 
+//	raca: "SRD"
+
+//nome: "Juba", 
+//	idade: 3, 
+//	raca: "SRD"
+
+//nome: "Jubo", 
+//	idade: 3, 
+//	raca: "SRD"
+
